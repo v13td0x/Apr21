75b0b0aa8102739b3a794f7d95b9842ceabafc75
v13td0x <0xdt314@gmail.com>
2021-04-29 13:55:25 +0700

    2

--- col.py
+++ col.py
@@ -1,6 +1,11 @@
 from pwn import *
 
-payload = p32(0x6c5cecc) + 4 * p32(0x6c5cec8)
 r = ssh(user = 'col', host = 'pwnable.kr', port = 2222, password = 'guest')
+#_______________________________________1__________________________________
+payload = p32(0x6c5cecc) + 4 * p32(0x6c5cec8)
 p = r.process(executable= './col', argv= ['col', payload])
 print(p.recv())
+#_______________________________________2__________________________________
+payload = b'\xc8\xce\xc5\x06'*4 + b'\xcc\xce\xc5\x06'
+p = r.process(['./col', payload])
+print(p.recvall())
